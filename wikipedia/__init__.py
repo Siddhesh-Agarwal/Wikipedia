c46df592ff54e13b54239ac2f4341542785339e1
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-05-12 17:43:03 +0530

    exact imports

--- wikipedia/__init__.py
+++ wikipedia/__init__.py
@@ -1,4 +1,23 @@
-from .wikipedia import *
-from .exceptions import *
+from .wikipedia import (
+    donate,
+    geosearch,
+    languages,
+    page,
+    random,
+    search,
+    set_lang,
+    set_user_agent,
+    set_rate_limiting,
+    suggest,
+    summary,
+    WikipediaPage,
+)
+from .exceptions import (
+    DisambiguationError,
+    HTTPTimeoutError,
+    PageError,
+    RedirectError,
+    WikipediaException,
+)
 
 __version__ = (1, 4, 0)
