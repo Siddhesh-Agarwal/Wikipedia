32789fb439ea099b386936981042dbdcef1559da
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-05-12 19:01:13 +0530

    fixed type hinting

--- wikipedia/util.py
+++ wikipedia/util.py
@@ -1,9 +1,12 @@
 import functools
 import sys
+from typing import Any, Callable, Dict, List
 
 
-def debug(fn):
-    def wrapper(*args, **kwargs):
+def debug(fn: Callable[[Any], Any]):
+    """Print the function call and return value"""
+
+    def wrapper(*args: List[Any], **kwargs: Dict[Any, Any]):
         print(fn.__name__, "called!")
         print(sorted(args), tuple(sorted(kwargs.items())))
         res = fn(*args, **kwargs)
@@ -14,19 +17,17 @@ def debug(fn):
 
 
 class cache(object):
+    """Cache the results of a function call"""
 
-    def __init__(self, fn):
+    def __init__(self, fn: Callable[[Any], Any]):
         self.fn = fn
-        self._cache = {}
+        self._cache: Dict[Any, Any] = {}
         functools.update_wrapper(self, fn)
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: List[Any], **kwargs: Dict[Any, Any]):
         key = str(args) + str(kwargs)
-        if key in self._cache:
-            ret = self._cache[key]
-        else:
-            ret = self._cache[key] = self.fn(*args, **kwargs)
-
+        ret = self._cache.get(key, self.fn(*args, **kwargs))
+        self._cache[key] = ret
         return ret
 
     def clear_cache(self):
@@ -34,6 +35,6 @@ class cache(object):
 
 
 # from http://stackoverflow.com/questions/3627793/best-output-type-and-encoding-practices-for-repr-functions
-def stdout_encode(u: str, default: str = "UTF8"):
+def stdout_encode(u: str, default: str = "utf-8") -> str:
     encoding = sys.stdout.encoding or default
     return u.encode(encoding, errors="replace").decode(encoding)
